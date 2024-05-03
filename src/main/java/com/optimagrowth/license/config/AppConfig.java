faa15813f4a37b2599f92830320ac54a0ccf9e65
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-05-03 10:56:48 -0400

    Remove unused imprts

--- src/main/java/com/optimagrowth/license/config/AppConfig.java
+++ src/main/java/com/optimagrowth/license/config/AppConfig.java
@@ -1,11 +1,7 @@
 package com.optimagrowth.license.config;
 
-import java.nio.charset.StandardCharsets;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 import com.optimagrowth.config.CrossCuttingConcernsConfig;
 
